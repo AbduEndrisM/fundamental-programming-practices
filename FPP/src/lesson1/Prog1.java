fc93dd4d4f859c8fb17ebfd8b30a022c9da0731d
Abdu Edris <beazsasm@gmail.com>
2018-11-26 23:52:36 -0600

    Fundamental Programming Practices

--- FPP/src/lesson1/Prog1.java
+++ FPP/src/lesson1/Prog1.java
@@ -1,10 +1,10 @@
 package lesson1;
- 
+
 public class Prog1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hello,World! 1111"); 
+		System.out.println("Hello, World!");
 	}
 
 }

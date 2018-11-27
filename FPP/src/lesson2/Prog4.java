a4a46110c14ed738d25ca8e3f6b041fae79aaf77
Abdu Edris <beazsasm@gmail.com>
2018-11-27 15:12:06 -0600

    Lesson – 2- Home Work Assignment

---
+++ FPP/src/lesson2/Prog4.java
@@ -0,0 +1,23 @@
+package lesson2;
+
+import java.util.Scanner;
+
+public class Prog4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter string");
+		
+		String hello = sc.nextLine();
+		
+		for (int i=hello.length()-1;i>=0;i--)
+		{
+			
+			System.out.print(hello.charAt(i));
+		}
+		
+	}
+
+}

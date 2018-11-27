17ea81cd3b12ddd8648472609433d1de1382a00a
Abdu Edris <beazsasm@gmail.com>
2018-11-27 14:29:07 -0600

    Week1-Lesson2-Assignment2

--- FPP/src/lesson1/Prog2.java
+++ FPP/src/lesson1/Prog2.java
@@ -4,8 +4,23 @@ public class Prog2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello, World! 2");
 
+		float x=1.27f;
+		float y=3.881f;
+		float z=9.6f;
+		
+		//System.out.println(x+y+z);
+		
+		// Question 1 cast 
+		int sum1=(int) (x+y+z);
+		System.out.println(sum1);
+		
+		
+		//Question 2 rounding
+		int sum2= Math.round(x+y+z);
+		System.out.println(sum2);
+		
 	}
+ 
 
 }

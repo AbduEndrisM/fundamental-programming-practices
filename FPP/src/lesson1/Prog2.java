70a80910d845dbe2a8915a1215ad0ce6afe12e41
Abdu Edris <beazsasm@gmail.com>
2018-11-26 23:56:07 -0600

    Week1-Lesson1-Assignment2

--- FPP/src/lesson1/Prog2.java
+++ FPP/src/lesson1/Prog2.java
@@ -4,8 +4,8 @@ public class Prog2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Hello, World! 2");
 
-System.out.println("Hello,World! 22222");
 	}
 
 }

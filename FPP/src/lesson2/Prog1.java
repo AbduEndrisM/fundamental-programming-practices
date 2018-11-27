fd97f9f8f7d0ec8d0cbb6d7f31e8e2044576a2d3
Abdu Edris <beazsasm@gmail.com>
2018-11-27 14:32:06 -0600

    Week1-Lesson2-Assignments

--- FPP/src/lesson2/Prog1.java
+++ FPP/src/lesson2/Prog1.java
@@ -1,10 +1,25 @@
 package lesson2;
 
+import java.util.Random;
+
 public class Prog1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello, World! 22");
+		Random r = new Random();
+		
+		// Question 1   1-9  
+		int a =r.nextInt(9-1+1)+1; // r.nextInt(max - min + 1) + min
+		double x= Math.pow(Math.PI, a);
+		System.out.println(x);
+		
+		
+		//Question 2	3-14
+		int b =r.nextInt(14-3+1)+3; // r.nextInt(max - min + 1) + min
+		double y= Math.pow(b, Math.PI);
+		System.out.println(y);
+		
+		
 	}
 
 }
--- FPP/src/lesson2/Prog3.java
+++ FPP/src/lesson2/Prog3.java
@@ -1,4 +1,4 @@
-package lesson1;
+package lesson2;
 
 public class Prog3 {
 

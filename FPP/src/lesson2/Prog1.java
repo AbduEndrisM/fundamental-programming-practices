13bc1e8f4a5955d2c964a60cb67c2aea0305380c
Abdu Edris <beazsasm@gmail.com>
2018-11-27 20:35:04 -0600

    Lesson – 2- Home Work Assignment

--- FPP/src/lesson2/Prog1.java
+++ FPP/src/lesson2/Prog1.java
@@ -6,12 +6,14 @@ public class Prog1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 		Random r = new Random();
 		
 		// Question 1   1-9  
 		int a =r.nextInt(9-1+1)+1; // r.nextInt(max - min + 1) + min
 		double x= Math.pow(Math.PI, a);
 		System.out.println(x);
+		
 		
 		
 		//Question 2	3-14

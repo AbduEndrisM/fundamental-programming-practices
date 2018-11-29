1888f50e4025db8860f49e3fc8d84e65420a246f
Abdu Edris <beazsasm@gmail.com>
2018-11-29 07:50:45 -0600

    Lesson – 3 - Home Work Assignment - 1

---
+++ FPP/src/Lesson3/Address.java
@@ -0,0 +1,50 @@
+package Lesson3;
+
+//Address, Customer, Main classes are for a single problem
+public class Address {
+
+	private String street;
+	private String city;
+	private String state;
+	private String zip;
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public String getZip() {
+		return zip;
+	}
+
+	Address(String street, String city, String state, String zip) {
+		this.street = street;
+		this.city = city;
+		this.state = state;
+		this.zip = zip;
+	}
+
+}

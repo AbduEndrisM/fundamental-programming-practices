1cfd03ee79bac4e4b5d77c1d210ecefa5fa416ef
Abdu Edris <beazsasm@gmail.com>
2018-11-29 07:52:34 -0600

    Lesson – 3 - Home Work Assignment - 1

--- FPP/src/lesson2/Prog6.java
+++ FPP/src/lesson2/Prog6.java
@@ -1,56 +1,46 @@
 package lesson2;
 
-import java.util.Scanner;
-
 public class Prog6 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		String[] string = new String[3]; 
-
 		
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter strings");
-		
-		
-		
-		for (int i=0; i<string.length;i++)
-		string[i]= sc.nextLine();
-		
-		int newlength = string.length;
-		int[] index = new int[5]; 
-		int count=0;
-		boolean dup=false;
-		
-		
-		for (int i=0; i<string.length;i++) {
-			for (int j=i+1; j<string.length;j++) {
-				
+			String[] inputArray = {"horse", "dog", "cat", "horse","dog", "cat"};
+			String[] tempArray = new String[inputArray.length];
+			int i = 0;
+			//outputArray[0] = inputArray[0];
+			boolean duplicateString = false;
+			for (String inputItem : inputArray) {
+				for (String outputItem : tempArray) {
+					if (outputItem == inputItem) {
+						duplicateString = true;
+					}
 			
-				if (string[i].equals(string[j])) {
-									
-				index[count]=i;  //
-				newlength--; //new array
-				count++;
-				}	
+				}
+				
+			if (!duplicateString) {
+				tempArray[i] = inputItem;
+				i++;
+				}
+			 }
+				
+			int nullItem = 0;
+				for (String str : tempArray) {
+					if (str == null) {
+						nullItem ++;
+					}
+				}
+				
+			String[] outputArray = new String[inputArray.length - nullItem];
+			for (int j = 0; j < outputArray.length; j++) {
+			outputArray[j] = tempArray[j];
 			}
-			 
+			for (String string : outputArray) {
+			System.out.println(string);
 			
 			
-		}
-		
+	}
 
-		String[] string2 = new String[newlength];
-		
-		for (int i=0; i<string.length;i++) {
-			if(i==)
-				
-			System.out.println(string2[i]);
-		}
-			
-		}
-				
-			
-
+}
 }

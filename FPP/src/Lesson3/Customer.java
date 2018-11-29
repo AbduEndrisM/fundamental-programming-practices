d13b8cd91e994f1a5d28b2ccc01fbf50cf158b4c
Abdu Edris <beazsasm@gmail.com>
2018-11-29 07:49:48 -0600

    Lesson – 3 - Home Work Assignment - 1

---
+++ FPP/src/Lesson3/Customer.java
@@ -0,0 +1,70 @@
+package Lesson3;
+
+public class Customer {
+
+	private String firstName;
+	private String lastName;
+	private String SSN;
+	
+	private Address billingAddress;
+	private Address shippingAddress;
+	
+	
+	public Customer(String x, String y, String z) {
+		firstName = x;
+		lastName = y;
+		SSN = z;
+	}
+
+	public String getfirstName() {
+		return firstName;
+	}
+
+	public String getlastName() {
+		return firstName;
+	}
+
+	public String getSNN() {
+		return SSN;
+	}
+
+	public Address getBillingAddress() {
+		return billingAddress;
+	}
+
+	public Address getShippingAddress() {
+		return shippingAddress;
+	}
+
+	public void setfirstName(String firstName) {
+		this.firstName=firstName;
+	}
+
+	public void setlastName(String lastName) {
+		this.lastName=lastName;
+	}
+
+	public void setSNN(String SSN) {
+		this.SSN=SSN;
+	}
+
+	public void setBillingAddress(Address billingAddress) {
+		this.billingAddress=billingAddress;
+	}
+
+	public void setShippingAddress(Address shippingAddress) {
+		this.shippingAddress=shippingAddress;
+	}
+
+	
+	String bill= billingAddress.getStreet() + " " + billingAddress.getCity()+ " " + billingAddress.getZip()+" "+ billingAddress.getState();
+	String ship= shippingAddress.getStreet() + " " + shippingAddress.getCity()+ " " + shippingAddress.getZip()+" "+ shippingAddress.getState();
+	
+	public String toString() {
+		return "[" + firstName + ", " + lastName + ", " + "ssn: " + SSN + "," +  bill+ ","+ship  +"]" ;
+		 
+		 }
+ 	
+	
+
+}
---
+++ Welcome/src/lesson3/Contact.java
@@ -0,0 +1,30 @@
+package lesson3;
+
+public class Contact {
+	String email;
+	String Mobile;
+	
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getMobile() {
+		return Mobile;
+	}
+
+	public void setMobile(String mobile) {
+		Mobile = mobile;
+	}
+
+	
+	Contact(String email, String Mobile){
+		this.email = email;
+		this.Mobile = Mobile;
+	}
+}
+
+

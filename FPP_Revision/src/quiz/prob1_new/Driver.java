4e040d571d2421137585793e057434ecd7e2334b
Abdu Edris <beazsasm@gmail.com>
2019-01-18 14:16:24 -0600

    FPP revision, Quiz - One

--- FPP_Revision/src/quiz/prob1_new/Driver.java
+++ FPP_Revision/src/quiz/prob1_new/Driver.java
@@ -2,23 +2,21 @@ package quiz.prob1_new;
 
 public class Driver {
 	public static void main(String[] args) {
-Figure[] objArr = {new HatMaker(),
-							new HatMaker(),
-							new VeeMaker(),
-							new ParallelMaker(),
-							new FaceMaker()};		
-		
-		new Driver(objArr);
-	}
-	public Driver(Figure[] figures){
+		Figure[] objArr = { new HatMaker(), new HatMaker(), new VeeMaker(), new ParallelMaker(), new FaceMaker() };
+
 		final String SPACE = " ";
+
+		for (Figure f : objArr)
+			System.out.print(f.getFigure() + SPACE);
 		
-		for (Figure f:figures)
-			System.out.print (f.getFigure() + SPACE);
-		 
+		//Way Two
+		/*
+		 * new Driver(objArr); }
+		 * 
+		 * public Driver(Figure[] figures) { final String SPACE = " ";
+		 * 
+		 * for (Figure f : figures) System.out.print(f.getFigure() + SPACE);
+		 */
 	}
 
 }
-  
-
-

ec88e3cfbad43c9c6682f3eee64eaea7992b6c39
Abdu Edris <beazsasm@gmail.com>
2019-01-19 00:23:54 -0600

    FPP revision, 3 - One

---
+++ FPP_Revision/src/three/one/AddElement.java
@@ -0,0 +1,30 @@
+package three.one;
+ 
+import java.util.Arrays;
+/**
+ *
+ * Add the element 9 at the position 1 
+ *
+ */
+public class AddElement {
+
+	public static void main(String[] args) {
+
+		int[] nums = new int[8];
+		
+		nums[0] = 4;
+		nums[1] = 5;
+		nums[2] = 7;
+		nums[3] = 1;
+		nums[4] = 3;
+		
+		System.out.println(Arrays.toString(nums));
+		
+		//implement
+		//added element 9 at pos 1: [4, 9, 5, 7, 1, 3, 0, 0]
+
+		
+
+	}
+
+}
---
+++ FPP_Revision/src/three/one/SmallestElement.java
@@ -0,0 +1,24 @@
+package three.one;
+
+ 
+import java.util.Arrays;
+
+/**
+ *
+ * swap the smallest element to 0th position in the array
+ *
+ */
+public class SmallestElement {
+
+	public static void main(String[] args) {
+
+		int[] nums = { 16, 35, 7, 1, -4, 36, 49 };
+		System.out.println(Arrays.toString(nums));
+
+		//implement
+
+		
+//		[-4, 35, 7, 1, 16, 36, 49] is the expected output.
+	}
+
+}

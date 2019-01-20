30d943898dee5ffda241b051d8e9f73c2fe76cd0
Abdu Edris <beazsasm@gmail.com>
2019-01-20 01:05:17 -0600

    FPP revision, Miscellaneous-Five

---
+++ FPP_Revision/src/miscellaneous/five/ProgFive.java
@@ -0,0 +1,38 @@
+package miscellaneous.five;
+
+public class ProgFive {
+
+	public static void main(String[] args) {
+		String x = "Hello";
+		String a = "Abdu";
+
+		for (int i = x.length() - 1; i >= 0; i--)
+			System.out.print(x.substring(i, i + 1));
+
+		StringBuilder y = new StringBuilder("Hello");
+		System.out.println();
+		System.out.println(y.reverse());
+
+		StringBuffer z = new StringBuffer("Hello");
+
+		System.out.println(z.reverse());
+
+		for (int i = a.length() - 1; i >= 0; i--)
+			System.out.print(a.charAt(i));
+
+		System.out.println();
+		// recursive
+
+		String rev = reverse(a);
+		System.out.println(rev);
+
+	}
+
+	private static String reverse(String z) {
+		if (z.isEmpty())
+			return z;
+		else
+			return reverse(z.substring(1)) + z.charAt(0);
+
+	}
+}

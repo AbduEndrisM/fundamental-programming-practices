a42788202e03fae32015318bbd031b0f16ddd15c
Abdu Edris <beazsasm@gmail.com>
2019-01-20 01:04:57 -0600

    FPP revision, Miscellaneous-Three

---
+++ FPP_Revision/src/miscellaneous/three/ProgOne.java
@@ -0,0 +1,17 @@
+package miscellaneous.three;
+
+public class ProgOne {
+
+	public static void main(String[] args) {
+		float x=1.27f;
+		float y=3.881f;
+		float z=9.6f;
+		
+		System.out.println((int)(x+y+z));
+		System.out.println(Math.round((x+y+z)));
+		
+		
+
+	}
+
+}

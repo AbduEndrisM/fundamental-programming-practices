4003a88e6f32534e309a0982d5c17008e87a13c1
Abdu Edris <beazsasm@gmail.com>
2019-01-20 01:05:27 -0600

    FPP revision, Miscellaneous-Six

---
+++ FPP_Revision/src/miscellaneous/six/removeDups.java
@@ -0,0 +1,18 @@
+package miscellaneous.six;
+
+public class removeDups {
+	public static void main(String[] args) {
+
+		String[] testData = {"horse", "dog", "cat", "horse","dog"};
+		
+		for (String s:testData)
+			System.out.print(s+", ");
+		removeDup(testData);
+		
+
+	}
+
+	private static void removeDup(String[] testData) {
+		
+	}
+}

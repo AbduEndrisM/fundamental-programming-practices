39ed482c7807a82f27f2da58ea3a25cb0193dff6
Abdu Edris <beazsasm@gmail.com>
2019-01-22 00:23:26 -0600

    FPP revision, 1 - Five - Three - classes of a class

---
+++ FPP_Revision/src/five/three/ClassClasses.java
@@ -0,0 +1,18 @@
+package five.three;
+
+public class ClassClasses {
+
+	public static void main(String[] args) {
+
+		Abdu a = new Abdu();
+		System.out.println(a);
+		System.out.println(a.getClass());
+		System.out.println(a.getClass().getName());
+		System.out.println(a.getClass().getSimpleName());
+	}
+
+}
+
+class Abdu{
+	
+}

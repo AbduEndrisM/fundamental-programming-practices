a687ba42fb2e7f64fb5f3c16e60b905ee4151836
Abdu Edris <beazsasm@gmail.com>
2019-01-24 02:46:01 -0600

    FPP revision, 4 - comparable and comparator

--- FPP_Revision/src/four/comparable/Employee.java
+++ FPP_Revision/src/four/comparable/Employee.java
@@ -1,8 +1,9 @@
 package four.comparable;
 
-import java.util.Arrays; 
+import java.util.Arrays;
+import java.util.Comparator;
 
-class Employee implements Comparable<Employee>{
+class Employee implements Comparable<Employee> {
 	String name;
 	int salary;
 
@@ -11,50 +12,94 @@ class Employee implements Comparable<Employee>{
 		this.salary = s;
 	}
 
-
-
 	@Override
 	public String toString() {
-		return "name=" + name + ", salary=" + salary ;
+		return "name=" + name + ", salary=" + salary;
 	}
-
-
 
 	public static void main(String[] args) {
 
-		String[] nameList = {"Harry","Bob","Steve","Jack"};
+		String[] nameList = { "Harry", "Bob", "Steve", "Jack" };
 
+		
 		Arrays.sort(nameList);
 		System.out.println(Arrays.toString(nameList));
-
-		int[] nums = {16,35,7,1,4};
+		
+			 
+		int[] nums = { 16, 35, 7, 1, 4 };
 		Arrays.sort(nums);
 		System.out.println(Arrays.toString(nums));
 
-		Employee[] emps = {new Employee("Pink", 200),new Employee("Gaga", 900),new Employee("Mike", 500)};
+		Employee[] emps = { new Employee("Pink", 200), new Employee("Gaga", 900), new Employee("Mike", 500),new Employee("Abdu", 500) };
 
+		//////////////////////////////////////////
+		System.out.println("Compare - Salary");
 		Arrays.sort(emps);
 		System.out.println(Arrays.toString(emps));
-		
 
+	
+		///////////////////////////////////
+		//By name
+		Arrays.sort(emps, new Comparator<Employee>() {
+
+			@Override
+			public int compare(Employee o1, Employee o2) {
+				// TODO Auto-generated method stub
+				return o1.name.compareTo(o2.name);
+			}
+			
+		});
+	System.out.println("Comparator - Name");
+	System.out.println(Arrays.toString(emps));
+	
+
+	//////////////////////////////////////////
+	//By salary
+	Arrays.sort(emps, new Comparator<Employee>() {
+
+		@Override
+		public int compare(Employee o1, Employee o2) {
+			// TODO Auto-generated method stub
+			return o1.salary - o2.salary;
+		}
+	});
+	
+	System.out.println("Comparator - Salary");
+	System.out.println(Arrays.toString(emps));
+
+	
+	
+	//////////////////////////////////////////
+	//By salary then name
+	Arrays.sort(emps, new Comparator<Employee>() {
+
+		@Override
+		public int compare(Employee o1, Employee o2) {
+			int x = o1.salary-o2.salary;
+			if (x==0)
+			return o1.name.compareTo(o2.name);
+			else
+				return x;
+		}
+	});
+	System.out.println("Comparator - Salary and name");
+	System.out.println(Arrays.toString(emps));
+
+	
+	
+	
 	}
-/*	  comparator not compare
-	public int compareByName(Employee o) { 
-		return (o.name.compareTo(name));
-		 
-	} 
-	public int compareBySalary(Employee o) { 
-		return (Integer.compare(o.salary, this.salary));
-			 
-	}*/
 
- 
 
+	// Salary
 	@Override
 	public int compareTo(Employee o) {
-		//return (o.name.compareTo(name));
+		// return (Integer)(this.salary-o.salary);
 		return (Integer.compare(this.salary, o.salary));
-		
-	} 
+
+	}
+	
+
+
 
 }

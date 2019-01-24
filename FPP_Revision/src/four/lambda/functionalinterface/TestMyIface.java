83f7817f0c30b0ba24ea50e8560df165d602ab28
Abdu Edris <beazsasm@gmail.com>
2019-01-23 21:17:33 -0600

    FPP revision, 4 - Lambda - functional interface

---
+++ FPP_Revision/src/four/lambda/functionalinterface/TestMyIface.java
@@ -0,0 +1,15 @@
+//package inner.anonymous.exercise.functionalinterface;
+
+package four.lambda.functionalinterface;
+import java.util.Random;
+
+public class TestMyIface {
+
+	public static void main(String[] args) {
+
+		//implement here
+		// an anonymous inner class, lambda expression
+	}
+
+
+}

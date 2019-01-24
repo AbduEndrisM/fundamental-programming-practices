3e1bfdf1f02165f6927abe742752dc179f793a13
Abdu Edris <beazsasm@gmail.com>
2019-01-23 22:43:22 -0600

    FPP revision, 4 - Lambda operations  operation - functioanl interface

---
+++ FPP_Revision/src/four/lambda/functionalinterface/MyIface.java
@@ -0,0 +1,21 @@
+//package inner.anonymous.exercise.functionalinterface;
+package four.lambda.functionalinterface;
+
+
+/**
+ *  Define your own functional interface that could be used as a wrapper
+ *  for a function that takes zero arguments and returns an integer.
+ 
+ *  Then use it to define an object that produces random numbers.
+ *  In a main method test your code;
+ *  then try to represent your  functional interface with an anonymous inner class, lambda expression
+ *
+ * 
+ *
+ */
+@FunctionalInterface
+public interface MyIface {
+
+	
+
+}

a14161533aa479523671718db7cc7addb481d2f4
Abdu Edris <beazsasm@gmail.com>
2019-01-23 22:45:49 -0600

    FPP revision, 6 - String operation using ArrayList and Linked List

---
+++ FPP_Revision/src/six/usinglist/ArrayListString.java
@@ -0,0 +1,32 @@
+package six.usinglist;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ArrayListString {
+
+	public static void main(String[] args) {
+
+		List<String> l = new ArrayList<>();
+//		List<String> l = new LinkedList()<>();
+		
+		l.add("Bob");
+		
+		System.out.println("The list of size " + l.size() + " is " + l);
+		l.add("Steve");
+		System.out.println("The list of size " + l.size() + " is " + l);
+		l.add("Susan");
+		System.out.println("The list of size " + l.size() + " is " + l);
+		l.add("Mark");
+		System.out.println("The list of size " + l.size() + " is " + l);
+		l.add("Dave");
+		System.out.println("The list of size " + l.size() + " is " + l);
+		l.remove("Mark");
+		System.out.println("The list of size " + l.size() + " is " + l);
+		l.remove("Bob");
+		System.out.println("The list of size " + l.size() + " is " + l);
+		
+	}
+
+}

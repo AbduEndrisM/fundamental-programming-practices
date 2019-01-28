4916d9901e6a5cdfc955220cd9042d0fe861d65e
Abdu Edris <beazsasm@gmail.com>
2019-01-27 22:58:07 -0600

    FPP Revision, 4 - Two - Lambda Functional Interface

--- FPP_Revision/src/four/lambda/functionalinterface/TestMyIface.java
+++ FPP_Revision/src/four/lambda/functionalinterface/TestMyIface.java
@@ -2,13 +2,26 @@
 
 package four.lambda.functionalinterface;
 import java.util.Random;
-
+ 
 public class TestMyIface {
 
 	public static void main(String[] args) {
 
 		//implement here
 		// an anonymous inner class, lambda expression
+		
+		MyIface obj = new MyIface() {
+			
+			@Override
+			public double getNumber() {
+
+				return  Math.random();
+			}
+		};
+	
+
+		System.out.println(obj.getNumber());
+	
 	}
 
 

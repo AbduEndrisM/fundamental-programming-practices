81a85cfb074524c6dcd62c06268660c4a51a41e4
Abdu Edris <beazsasm@gmail.com>
2019-01-27 22:57:24 -0600

    FPP Revision, 5 - One - equals

---
+++ FPP_Revision/src/five/equals/case3/Person.java
@@ -0,0 +1,49 @@
+package five.equals.case3;
+
+import java.time.LocalDate;
+
+
+public class Person {
+
+	private String name;
+	private LocalDate dateOfBirth;
+	Person(String name, int year, int month, int dayOfMonth) {
+		this.name = name;
+		dateOfBirth = LocalDate.of(year, month, dayOfMonth);
+	}
+	public String getName() {
+		return name;
+	}
+	
+	
+	public LocalDate getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+	    if (obj == this) {
+	      return true;
+	    }
+	    if (this.getClass()!=(obj.getClass())) {
+	      return false;
+	    }
+	    
+	    Person cc = (Person)obj;
+	    boolean isEqual = (this.name.equals(cc.name))&&(this.dateOfBirth.equals(cc.dateOfBirth));
+	    return isEqual;
+	  }
+	
+	 
+		
+	public static void main(String[] args) {
+
+		Person p = new Person("Joe", 2018, 10, 21);
+		PersonWithJob p1 = new PersonWithJob("Joe", 2018, 10, 21, 500);
+		PersonWithJob p2 = new PersonWithJob("Joe", 2018, 10, 21, 800);
+		
+		System.out.println(p.equals(p2));
+		System.out.println(p1.equals(p2));
+	}
+
+}

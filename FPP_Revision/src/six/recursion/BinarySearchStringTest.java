4c8100f59654812d7baa0bb0a8f970374cd365d5
Abdu Edris <beazsasm@gmail.com>
2019-01-28 21:40:17 -0600

    FPP Revision, 6 - Recursion - Binary search - string

---
+++ FPP_Revision/src/six/recursion/BinarySearchStringTest.java
@@ -0,0 +1,21 @@
+package six.recursion;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BinarySearchStringTest {
+
+	@Test
+	public void test() {
+		BinarySearchString test = new BinarySearchString();
+		
+		boolean x = test.binarySearch("abdu", 'b', 0, 4);
+		assertEquals(true, x);
+		
+		boolean y = test.binarySearch("abdu", 'A', 0, 4);
+		assertEquals(false, y);
+		
+	}
+
+}

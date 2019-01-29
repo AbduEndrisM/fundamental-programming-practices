0e5efe0c1b65c98842b4c0f23f90ff5ff1785937
Abdu Edris <beazsasm@gmail.com>
2019-01-28 23:36:22 -0600

    FPP Revision, 6 - Recursion - merge strings in sorted order

--- FPP_Revision/src/six/recursion/MergeString.java
+++ FPP_Revision/src/six/recursion/MergeString.java
@@ -2,8 +2,42 @@ package six.recursion;
 
 public class MergeString {
 
-	public static void main(String[] args) { 
+	public static void main(String[] args) {
 
+		String string1 = "abf";
+		String string2 = "bdmz";
+
+		String string = merge(string1, string2);
+		System.out.println(string);
+
+	}
+
+	private static String merge(String string1, String string2) {
+
+//		String x = string1.substring(1);
+//		String y = string2.substring(1);
+
+		String z = "";
+
+		if (string1.length() ==0||string2.length() ==0) {
+			if(string1.length()==0)
+				return z+string2;
+			else
+				return z+string1;
+		}else {
+			if (string1.charAt(0) < string2.charAt(0)) {
+				z = z + string1.charAt(0);
+			 
+				return z+ merge( string1.substring(1), string2);
+			} else {
+				z = z + string2.charAt(0);
+				 
+				return z + merge(string1, string2.substring(1));
+			}
+		}
+			
+		
+		
 	}
 
 }

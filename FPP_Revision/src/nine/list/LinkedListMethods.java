1d0f1c12e97077f32be1b728404d6ca39acb4914
Abdu Edris <beazsasm@gmail.com>
2019-01-29 21:51:38 -0600

    FPP Revision, 9 - LinkeldList methods

---
+++ FPP_Revision/src/nine/list/LinkedListMethods.java
@@ -0,0 +1,12 @@
+package nine.list;
+
+public interface LinkedListMethods {
+	public void addLast(String element);
+	public void addFirst(String element);
+	public void add(int index, String element);
+	public String get(int index);
+	public boolean isEmpty();
+	public String toString();
+	public int size();
+
+}

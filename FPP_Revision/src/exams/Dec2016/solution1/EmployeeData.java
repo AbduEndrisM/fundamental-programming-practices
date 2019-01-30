0e33e0badeddfcb9de989865ba50bb9555cd72b9
Abdu Edris <beazsasm@gmail.com>
2019-01-29 21:49:51 -0600

    FPP revision, Exam-Dec2016 - solution 1

---
+++ FPP_Revision/src/exams/Dec2016/solution1/EmployeeData.java
@@ -0,0 +1,6 @@
+package exams.Dec2016.solution1;
+
+public interface EmployeeData {
+	public double getSalary();
+
+}
---
+++ FPP_Revision/src/exams/Dec2016/solution1/Statistics.java
@@ -0,0 +1,17 @@
+package exams.Dec2016.solution1;
+import java.util.*;
+public class Statistics {
+	/** 
+	 * Polymorphically computes and returns the sum
+	 * of all the salaries of all the staff/teachers in the list.
+	 */
+	public static double computeSumOfSalaries(List<EmployeeData> aList) {
+		
+		double sum=0.0;
+		for(EmployeeData e:aList)
+			sum=sum+e.getSalary();
+		return sum;
+		//implement
+		//compute sum of all salaries of people in aList and return
+	}
+}

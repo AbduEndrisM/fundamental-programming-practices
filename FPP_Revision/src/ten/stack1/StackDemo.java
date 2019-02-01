9a551fa90c6dab7e87241683f202997861bb9c06
Abdu Edris <beazsasm@gmail.com>
2019-01-31 21:29:06 -0600

    FPP Revision, 10 - Stack -

---
+++ FPP_Revision/src/ten/stack1/StackDemo.java
@@ -0,0 +1,19 @@
+package ten.stack1;
+
+import java.util.Stack;
+
+public class StackDemo {
+
+	public static void main(String[] args) {
+
+		Stack<Integer> stack = new Stack();
+		
+		stack.push(5);
+		stack.push(7);
+		stack.push(1);
+		 
+		for(int i:stack)
+			System.out.println(i);
+ 	} 
+
+}

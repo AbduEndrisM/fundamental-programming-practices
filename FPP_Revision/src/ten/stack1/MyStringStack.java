17de7a40b974555e033765f4ce0f95becd4f63ca
Abdu Edris <beazsasm@gmail.com>
2019-02-02 04:03:59 -0600

    FPP Revision, 10 - Stack -

--- FPP_Revision/src/ten/stack1/MyStringStack.java
+++ FPP_Revision/src/ten/stack1/MyStringStack.java
@@ -4,6 +4,11 @@ package ten.stack1;
 
 	public void push(String string);
 	public String pop();
-	public String peek();
-	
+	public String peek(); 
+	public void resize();
+	public boolean isFull();
+	public int size();
+	public boolean isEmpty();
+	public void clear();
+	 
 }

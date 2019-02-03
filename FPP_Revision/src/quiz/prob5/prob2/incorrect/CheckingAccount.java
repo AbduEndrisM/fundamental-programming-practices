942f12f9f9534132e95ed1fc51b26c1e075808e8
Abdu Edris <beazsasm@gmail.com>
2019-02-02 22:25:09 -0600

    FPP revision, Quiz - Polymorphism

---
+++ FPP_Revision/src/quiz/prob5/prob2/incorrect/CheckingAccount.java
@@ -0,0 +1,11 @@
+package quiz.prob5.prob2.incorrect;
+
+public final class CheckingAccount {
+	public CheckingAccount(double balance) {
+		this.balance = balance;
+	}
+	double balance;
+	public double getBalance() {
+		return balance;
+	}
+}

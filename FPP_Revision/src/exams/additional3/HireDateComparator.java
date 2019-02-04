11740bcaae8d92359fb7e8df6a068de497373137
Abdu Edris <beazsasm@gmail.com>
2019-02-03 21:12:09 -0600

    FPP Revision, Exam-Additional - Polymorphsim

---
+++ FPP_Revision/src/exams/additional3/HireDateComparator.java
@@ -0,0 +1,12 @@
+package exams.additional3;
+
+
+import java.util.Comparator;
+
+public class HireDateComparator implements Comparator<Employee> {
+	//compareTo in this case is not consistent with equals
+	public int compare(Employee e1, Employee e2) {
+		return e1.getHireDate().compareTo(e2.getHireDate());
+	}
+	
+}
---
+++ FPP_Revision/src/exams/additional3/NameComparator.java
@@ -0,0 +1,13 @@
+package exams.additional3;
+
+import java.util.Comparator;
+
+
+
+public class NameComparator implements Comparator<Employee> {
+	//compareTo in this case is not consistent with equals
+	public int compare(Employee e1, Employee e2) {
+		return e1.getName().compareTo(e2.getName());
+	}
+	
+}

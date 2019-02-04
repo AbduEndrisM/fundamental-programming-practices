934a5a5fd522ac631ef2abdf0f36ccf085edb979
Abdu Edris <beazsasm@gmail.com>
2019-02-03 20:57:47 -0600

    FPP Revision, Exam-June2017 - polymorphism

---
+++ FPP_Revision/src/exams/June2017/prob2/Admin.java
@@ -0,0 +1,25 @@
+package exams.June2017.prob2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Admin {
+	public static List<Student>/*<implement>*/ convertArray(Student[] studentArray) {
+		/* implement */
+		List<Student>list = new ArrayList<>();
+		list.addAll( Arrays.asList(studentArray));
+		return list;
+	}
+	
+	
+	public static double computeAverageGpa(List<Student>/*<implement>*/ studentList) {
+		/*implement */
+		double gpa=0.0;
+		for(Student s:studentList) {
+			gpa=gpa+s.computeGpa();
+		}
+		gpa=gpa/studentList.size();
+		return gpa;
+	}
+}
---
+++ FPP_Revision/src/exams/June2017/prob2/Student.java
@@ -0,0 +1,6 @@
+package exams.June2017.prob2;
+
+abstract public class Student {
+//implement
+	public abstract double computeGpa();
+}

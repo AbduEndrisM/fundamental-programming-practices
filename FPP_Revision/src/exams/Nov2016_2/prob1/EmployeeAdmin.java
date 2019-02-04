52085b374ebf89cb2ef8e71a0c5fb5745752e9b5
Abdu Edris <beazsasm@gmail.com>
2019-02-03 21:14:07 -0600

    FPP Revision, Exam-November2016

---
+++ FPP_Revision/src/exams/Nov2016_2/prob1/EmployeeAdmin.java
@@ -0,0 +1,30 @@
+package exams.Nov2016_2.prob1;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class EmployeeAdmin {
+	
+	/**
+		Returns a list of Employees whose social security number is on the input list socSecNums
+		and whose salary is > 80000. The list must be ordered by social security number,
+		from lowest to highest. To sort, you must use a Collections sorting method
+		and you must define your own Comparator to be used to compare Employees by ssn. 
+	*/
+	public static List<Employee> prepareReport(HashMap<String, Employee> table, List<String> socSecNums) {
+		//IMPLEMENT
+		List<Employee> list = new ArrayList<>();
+		List<String> keys = new ArrayList<>();
+		keys.addAll(table.keySet());
+		
+		for(String s:keys) {
+			if (socSecNums.contains(s) && table.get(s).getSalary()>80000) {
+				list.add(table.get(s));
+			}
+		}
+		return list;
+		
+	}
+	
+}

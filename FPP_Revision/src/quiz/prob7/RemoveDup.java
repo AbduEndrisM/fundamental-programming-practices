589f77760cf3e102b2199c963ed36105ca59d677
Abdu Edris <beazsasm@gmail.com>
2019-02-04 23:37:18 -0600

    FPP Revision, Quiz - Seven -  remove duplicate strings using HasMap

---
+++ FPP_Revision/src/quiz/prob7/RemoveDup.java
@@ -0,0 +1,38 @@
+package quiz.prob7;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+import javax.xml.soap.SAAJResult;
+
+public class RemoveDup {
+
+	public static void main(String[] args) {
+
+		String[] testData = { "horse", "dog", "cat", "horse", "dog" };
+		String[] result = removeDups(testData);
+
+		System.out.println(Arrays.toString(result));
+	}
+
+	private static String[] removeDups(String[] data) {
+
+		// remove duplicate elements in an array using HashMap
+		HashMap<String, String> hash = new HashMap<>();
+		for (String s : data) {
+			hash.put(s, null);
+		}
+
+		String[] s = new String[hash.keySet().size()];
+				s=hash.keySet().toArray(new String[0]);
+				return s;
+/*		for (int i = 0; i < hash.keySet().size(); i++) {
+			
+			s[i] =  (String) hash.keySet().toArray()[i];
+		}
+		return s;
+*/
+	}
+
+}

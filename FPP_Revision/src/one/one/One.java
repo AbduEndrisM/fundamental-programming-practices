0e96f3a3b0c3dd1c7840525c3e5810161c6097e3
Abdu Edris <beazsasm@gmail.com>
2019-02-05 23:57:58 -0600

    FPP revision, 1 - One - Arrays

--- FPP_Revision/src/one/one/One.java
+++ FPP_Revision/src/one/one/One.java
@@ -1,46 +1,41 @@
 package one.one;
 
 public class One {
-public static void main(String[] args) {
-	int[] nums = {2,4,3,8,10,11,14,16,17,20};
-  
-	for(int i=0;i<nums.length;i=i+2)
-		System.out.println("Even Index Number "+i +":  "+ nums[i]);
-  
-	for(int i=0;i<nums.length;i++)
-		if(nums[i]%2==0)
-		System.out.println("Even numbers "+ nums[i]);
-  
-	
-	// using ternary operator 
-	for(int i=0;i<nums.length;i++) {
-		System.out.println("Number: "+ nums[i]+"->"+ (nums[i]%2==0?"Even":"Odd"));
-		//System.out.println(nums[i]%2==0?"Even":"Odd");
-	}
+	public static void main(String[] args) {
+		int[] nums = { 2, 4, 3, 8, 10, 11, 14, 16, 17, 20 };
 
+		for (int i = 0; i < nums.length; i = i + 2)
+			System.out.println("Even Index Number " + i + ":  " + nums[i]);
 
-	
-	
+		for (int i = 0; i < nums.length; i++)
+			if (nums[i] % 2 == 0)
+				System.out.println("Even numbers " + nums[i]);
+
+		// using ternary operator
+		for (int i = 0; i < nums.length; i++) {
+			System.out.println("Number: " + nums[i] + "->" + (nums[i] % 2 == 0 ? "Even" : "Odd"));
+			// System.out.println(nums[i]%2==0?"Even":"Odd");
+		}
+
 		Employee a = new Employee("Alice");
 		Employee b = new Employee("Bob");
-		swap(a,b);
-		System.out.println(a+" "+b);
-		
-	int num = 50;
-	triple(num);
+		swap(a, b);
+		System.out.println(a + " " + b);
 
-	System.out.println(num);
-	
-	
-}
+		int num = 50;
+		triple(num);
+
+		System.out.println(num);
+
+	}
 
 	private static void swap(Employee x, Employee y) {
-	// 
+		//
 		Employee temp = x;
-		 x = y;
-			 y = temp;
-	
-}
+		x = y;
+		y = temp;
+
+	}
 
 	public static void triple(int x) {
 		x = 3 * x;
@@ -58,8 +53,5 @@ class Employee {
 	public String toString() {
 		return "Employee [x=" + x + "]";
 	}
-	
-	
 
-			
 }

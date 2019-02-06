37ac8dab59d098f46abf9b9a1289b0fb60a62384
Abdu Edris <beazsasm@gmail.com>
2019-02-05 23:55:24 -0600

    FPP Revision, 5 - Three - Reflection classes

--- FPP_Revision/src/five/three/ClassClasses.java
+++ FPP_Revision/src/five/three/ClassClasses.java
@@ -1,18 +1,21 @@
 package five.three;
-
+/*Reflection is an API which is used to examine or modify 
+the behavior of methods, classes, interfaces at runtime.
+*/
 public class ClassClasses {
 
 	public static void main(String[] args) {
 
 		Abdu a = new Abdu();
+		Abdu aa = new Abdu();
 		System.out.println(a); 
+		System.out.println(aa); 
 		System.out.println(a.getClass());
 		System.out.println(a.getClass().getName());
-		System.out.println(a.getClass().getSimpleName());
+		System.out.println(a.getClass().getSimpleName()); 
 	}
 
 }
 
-  class Abdu{
-	
+  class Abdu{ 
 }

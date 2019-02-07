1e9756dcab5f7927e9a29e0655765fafe118b6bb
Abdu Edris <beazsasm@gmail.com>
2019-02-06 18:19:11 -0600

    FPP revision, 10 - Queue - LinkeList Implementation

---
+++ FPP_Revision/src/ten/arraylinkedlistnode/LinkedListQueueImpl.java
@@ -0,0 +1,63 @@
+package ten.arraylinkedlistnode;
+
+import java.util.LinkedList;
+
+public class LinkedListQueueImpl {
+	
+	LinkedList<String> arr;
+	
+	
+	public LinkedListQueueImpl() {
+		arr = new LinkedList<String>();
+	}
+
+	public String peek() {
+		return arr.getFirst().toString();
+	}
+
+	public void push(String obj) {
+		arr.addLast(obj);
+	}
+
+	public void pop() {
+		arr.removeFirst();
+		System.out.println("popping..");
+		
+	}
+
+	public boolean isEmpty() {
+		return arr.isEmpty();
+	}
+
+	public int size() {
+		return arr.size();
+	}
+
+	@Override
+	public String toString() {
+		return "LinkedListStackImpl [arr=" + arr + "]";
+	}
+
+	public static void main(String[] args) {
+
+		LinkedListQueueImpl names = new LinkedListQueueImpl();
+
+		names.push("Ravi");  
+		names.push("Vijay");  
+		names.push("Muu");  
+		names.push("Ajay");
+		System.out.println(names);
+
+		names.pop();
+		names.pop();
+		System.out.println(names);
+
+		System.out.println("peeking...." + names.peek());
+		names.push("Pont");
+		System.out.println(names);
+
+		System.out.println("size: " + names.size());
+
+	}
+	
+}

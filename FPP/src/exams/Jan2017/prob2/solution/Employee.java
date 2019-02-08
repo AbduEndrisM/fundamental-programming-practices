18aeaf6ff3dfb73c944c19174887e99d278bc1f4
Abdu Edris <beazsasm@gmail.com>
2019-02-08 11:20:48 -0600

    FPP - Standard exam - Jan 2017

--- FPP/src/exams/Jan2017/prob2/solution/Employee.java
+++ FPP/src/exams/Jan2017/prob2/solution/Employee.java
@@ -3,22 +3,22 @@ package exams.Jan2017.prob2.solution;
 import java.util.List;
 
 public class Employee {
-	public Employee(String id, List accounts) {
+	public Employee(String id, List<Account> accounts) {
 		this.id = id;
 		this.accounts = accounts;
 	}
 	String id;
-	List accounts;
+	List<Account>accounts;
 	public String getId() {
 		return id;
 	}
 	public void setId(String id) {
 		this.id = id;
 	}
-	public List getAccounts() {
+	public List<Account> getAccounts() {
 		return accounts;
 	}
-	public void setAccounts(List accounts) {
+	public void setAccounts(List<Account> accounts) {
 		this.accounts = accounts;
 	}
 }

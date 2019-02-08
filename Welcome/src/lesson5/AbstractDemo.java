c96d719f5e89ddf6cb0167418ddf2050af838d6a
Abdu Edris <beazsasm@gmail.com>
2019-02-08 11:16:52 -0600

    Lesson 5 - Abstract class demo

--- Welcome/src/lesson5/AbstractDemo.java
+++ Welcome/src/lesson5/AbstractDemo.java
@@ -1,16 +1,25 @@
 package lesson5;
- 
 
-abstract class Base { 
-    Base() { System.out.println("Base Constructor Called"); } 
-    abstract void fun(); 
-} 
-class Derived extends Base { 
-    Derived() { System.out.println("Derived Constructor Called"); } 
-    void fun() { System.out.println("Derived fun() called"); } 
-} 
-class AbstractDemo { 
-    public static void main(String args[]) {  
-       Base d = new Derived(); 
-    } 
-} 
+abstract class Base {
+	Base() {
+		System.out.println("Base Constructor Called");
+	}
+
+	abstract void fun();
+}
+
+class Derived extends Base {
+	Derived() {
+		System.out.println("Derived Constructor Called");
+	}
+
+	void fun() {
+		System.out.println("Derived fun() called");
+	}
+}
+
+class AbstractDemo {
+	public static void main(String args[]) {
+		Base d = new Derived();
+	}
+}

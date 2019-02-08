68d88bf19e365cee9e5add5846562d3b9ddb0f7a
Abdu Edris <beazsasm@gmail.com>
2019-02-08 11:25:23 -0600

    Miscellaneous-HashMap demo

---
+++ Miscellaneous/src/exams/hashmap/Employee.java
@@ -0,0 +1,66 @@
+package exams.hashmap;
+  
+public class Employee {
+
+	String fName;
+	String lName;
+	int salary; 
+ 
+	public Employee(String fName, String lName, int salary) {
+		super();
+		this.fName = fName;
+		this.lName = lName;
+		this.salary = salary;
+	}
+
+	public String getfName() {
+		return fName;
+	}
+
+	public void setfName(String fName) {
+		this.fName = fName;
+	}
+
+	public String getlName() {
+		return lName;
+	}
+
+	public void setlName(String lName) {
+		this.lName = lName;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((lName == null) ? 0 : lName.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Employee other = (Employee) obj;
+		if (lName == null) {
+			if (other.lName != null)
+				return false;
+		} else if (!lName.equals(other.lName))
+			return false;
+		return true;
+	} 
+	
+	
+}

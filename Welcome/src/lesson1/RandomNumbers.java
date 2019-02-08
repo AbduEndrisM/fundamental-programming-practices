6068118f4aa283b98e17d55fe783af83f32a4e9a
Abdu Edris <beazsasm@gmail.com>
2019-02-08 11:11:44 -0600

    Lesson 1 - Random numbers demo

--- Welcome/src/lesson1/RandomNumbers.java
+++ Welcome/src/lesson1/RandomNumbers.java
@@ -1,30 +1,32 @@
- 
+
 package lesson1;
+
 import java.util.Random;
 
 /**
- *  This class keeps hold of an instance of Random and guarantees
- *  that the same instance will be used over and over (so the cost
- *  of re-seeding the random number generator is avoided).
- *  Each time the getRandomInt() method is called, the next integer
- *  from this Random instance is returned. 
+ * This class keeps hold of an instance of Random and guarantees that the same
+ * instance will be used over and over (so the cost of re-seeding the random
+ * number generator is avoided). Each time the getRandomInt() method is called,
+ * the next integer from this Random instance is returned.
  */
 public class RandomNumbers {
-    
-	public static void main(String[] args){
+
+	public static void main(String[] args) {
 		Random r = new Random();
-		System.out.println(r.nextInt()+" "+r.nextInt()+" "+r.nextInt()+" "+r.nextInt());
+		System.out.println(r.nextInt() + " " + r.nextInt() + " " + r.nextInt() + " " + r.nextInt());
 		r = new Random();
-		System.out.println(r.nextInt()+" "+r.nextInt()+" "+r.nextInt()+" "+r.nextInt());
+		System.out.println(r.nextInt() + " " + r.nextInt() + " " + r.nextInt() + " " + r.nextInt());
 		r = new Random();
-		System.out.println(r.nextInt()+" "+r.nextInt()+" "+r.nextInt()+" "+r.nextInt());
+		System.out.println(r.nextInt() + " " + r.nextInt() + " " + r.nextInt() + " " + r.nextInt());
 		r = new Random();
-		System.out.println(r.nextInt()+" "+r.nextInt()+" "+r.nextInt()+" "+r.nextInt());
+		System.out.println(r.nextInt() + " " + r.nextInt() + " " + r.nextInt() + " " + r.nextInt());
+
+		System.out.println(r.nextInt((9 - 1 + 1) + 1));
+
 		String s = "hello";
 		char c = s.charAt(0);
 		char d = s.charAt(1);
-		System.out.println(c+" "+d+" "+(char)(c+d));
+		System.out.println(c + " " + d + " " + (char) (c + d));
 	}
-        
-        
+
 }

a4ca3b449c6cc7e0cfa0bf68ab0490583822a8dc
Abdu Edris <beazsasm@gmail.com>
2019-02-08 11:24:52 -0600

    Miscellaneous-Count number of elements of a stack and display in HashTable

---
+++ Miscellaneous/src/exams/hashmap/CountDuplicate.java
@@ -0,0 +1,42 @@
+package exams.hashmap;
+ 
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Stack;
+
+public class CountDuplicate {
+
+	public static void main(String[] args) {
+
+		Stack <Employee> emp = new Stack<>();
+		
+		emp.push(new Employee("abduA", "edris", 1111));
+		emp.push(new Employee("abduB", "edris", 2222));
+		emp.push(new Employee("abduC", "edrisM", 3333));
+		emp.push(new Employee("abduD", "edris", 444));
+		emp.push(new Employee("abduC", "edrisM", 3333));
+		emp.push(new Employee("abduD", "edris", 444));
+		emp.push(new Employee("abduC", "edrisa", 3333));
+		emp.push(new Employee("abduD", "edris", 444));
+		
+		
+		
+		HashMap <String, Integer> table = new HashMap<>();
+		  
+		int count =0;
+		for(Employee e:emp) {
+			if(table.keySet().contains(e.lName)) {
+				count++;
+			table.put(e.lName, table.get(e.lName)+1);
+		}
+			else {
+				count=1;
+				table.put(e.lName,count);
+
+			}
+	}
+System.out.println(table);
+}
+}

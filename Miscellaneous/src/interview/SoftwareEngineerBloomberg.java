af075d1313cdaa5717d1060771546e60cc50277c
Abdu Edris <beazsasm@gmail.com>
2019-03-01 03:30:29 -0600

    Interview - Software Engineer

---
+++ Miscellaneous/src/interview/SoftwareEngineerBloomberg.java
@@ -0,0 +1,16 @@
+package interview;
+
+/*Interview One
+Q1 Josephus problem
+Q2 Find all anagrams of a given word in quickest way possible
+Interview Two
+Reverse a linked list*/
+
+public class SoftwareEngineerBloomberg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

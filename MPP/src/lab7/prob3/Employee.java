59f4d0ffe12ab2223036af6633b3c01b165f9fd4
AbduEndrisM <beazsasm@gmail.com>
2019-03-14 03:27:13 -0500

    MPP Labs - Homework - sorted and comparing

---
+++ MPP/src/lab7/prob3/Employee.java
@@ -0,0 +1,63 @@
+package lab7.prob3;
+
+import java.time.format.ResolverStyle;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import static java.util.Comparator.comparing;
+
+
+public class Employee {
+	String name;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getSalary() {
+		return salary;
+	}
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+	int salary;
+	public Employee(String n, int s) {
+		this.name = n;
+		this.salary = s;
+	}
+	@Override
+	public String toString() {
+		return "[" + name + ", " + salary+"]";
+	}
+	
+	public static void main(String[] args) {
+		List<Employee> list = new ArrayList<Employee>() {
+			{
+				add(new Employee("Joe", 50000));
+				add(new Employee("Jim", 75000));
+				add(new Employee("Tom", 80000));
+				add(new Employee("Jim", 70000));
+				add(new Employee("Steve", 55000));
+				add(new Employee("Jim", 100000));
+				add(new Employee("Joe", 59000));
+				add(new Employee("Rich", 88000));
+			}
+		};
+		//expected output:
+		//[[Jim, 100000], [Jim, 75000], [Jim, 70000], [Joe, 59000], [Joe, 50000], [Rich, 88000], [Steve, 55000], [Tom, 80000]]
+		
+		
+//			.sorted((o1, o2)->o1.getName().compareTo(o2.getName()))
+		List<Employee>list2 = list.stream()
+							.sorted(Comparator.comparing(Employee::getName)
+							.thenComparing(comparing(Employee::getSalary).reversed()))
+	                       .collect(Collectors.toList());
+		
+
+		System.out.println(list2/*implement */);
+
+	}
+}
+
